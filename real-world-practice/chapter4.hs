72888017beeace39f620e6a3257b8603b2a51c35
Mateusz Szotowicz <m.szotowicz@softline.com.pl>
2018-02-16 15:33:27 +0100

    Chapter 4 next part

--- real-world-practice/chapter4.hs
+++ real-world-practice/chapter4.hs
@@ -1,5 +1,10 @@
+import Data.Char (digitToInt, isDigit)
 import qualified Data.List as List
+
+type ErrorMessage = String
 --Chapter 4
+
+-- A --
 
 -- 1
 safeHead :: [a] -> Maybe a
@@ -34,3 +39,28 @@ firstWords input = foldr (\x acc -> (head . words $ x) : acc) [] (lines input)
 -- 4
 stringTranspose :: String -> String
 stringTranspose = unlines . List.transpose . lines
+
+
+-- B --
+
+-- 1
+asInt_fold :: String -> Int
+asInt_fold [] = 0
+asInt_fold ('-':input) = negate . asInt_fold $ input
+asInt_fold input = List.foldl' consume 0 input
+	where consume acc x
+		| x == '.'	= error "Char.digitToInt: not a digit '.'"
+		| otherwise	= acc * 10 + (digitToInt x)
+		
+-- 2
+asInt_either :: String -> Either ErrorMessage Int
+asInt_either [] = Right 0
+asInt_either ('-':input) = case asInt_either $ input of
+	Left msg -> Left msg
+	Right x -> Right (negate x)
+asInt_either input = List.foldl' consume (Right 0) input
+	where
+	consume (Left acc) x = Left acc
+	consume (Right acc) x
+		| isDigit x = Right (acc * 10 + (digitToInt x))
+		| otherwise = Left ("non-digit '" ++ x : "'")
