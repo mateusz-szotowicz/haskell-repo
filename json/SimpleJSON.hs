39ab7331cd125d4c21cdac4c58e0fd20423422d5
Mateusz Szotowicz <m.szotowicz@softline.com.pl>
2018-03-14 15:33:47 +0100

    Whitespace fix

--- json/SimpleJSON.hs
+++ json/SimpleJSON.hs
@@ -39,12 +39,12 @@ renderJValue (JBool False) = "false"
 renderJValue JNull = "null"
 
 renderJValue (JObject o) = "{" ++ pairs o ++ "}"
-	where
-		pairs [] = ""
-		pairs ps = intercalate ", " (map renderPair ps)
-		renderPair (k, v) = show k ++ ": " ++ renderJValue v
-		
+    where
+        pairs [] = ""
+        pairs ps = intercalate ", " (map renderPair ps)
+        renderPair (k, v) = show k ++ ": " ++ renderJValue v
+        
 renderJValue (JArray a) = "[" ++ values a ++ "]"
-	where
-		values [] = ""
-		values vs = intercalate ", " (map renderJValue vs)
+    where
+        values [] = ""
+        values vs = intercalate ", " (map renderJValue vs)
